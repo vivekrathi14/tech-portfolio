e2d7adb320593e4d78a02dd881a8dedd0a27c77c
Vivek Rathi <vivek.rathi14@gmail.com>
2021-01-07 12:18:42 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -72,9 +72,9 @@ const siteConfig = {
     If you want to remove a social media icon, you can add the two // before the line
     or delete the line. */
     socialMedia: {
-        linkedin: 'https://www.linkedin.com/in/',
-        twitter: 'https://twitter.com/',
-        github: 'https://github.com/',
+        linkedin: 'https://www.linkedin.com/in/vivekrathi14',
+//         twitter: 'https://twitter.com/',
+        github: 'https://https://github.com/vivekrathi14',
         //facebook: 'https://www.facebook.com/',
         //instagram: 'https://www.instagram.com/',
         //youtube: 'https://www.youtube.com/',
